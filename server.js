0f22ec3c999bfcdfcd4fd8e8da36c38718ed9784
Adnan Haider <118106335+AdnanMehr8@users.noreply.github.com>
2024-05-05 18:56:39 +0500

    changes

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+import cookieParser from "cookie-parser";
+import http from "http";
+
 const mongoose = require('mongoose');
 const path = require('path');
 require('dotenv').config();
@@ -10,22 +13,28 @@ const portfolioRoute = require('./routes/portfolioRoutes');
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 
-mongoose.connect(process.env.mongo_url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
 
 app.use('/api/portfolio', portfolioRoute);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'));
-  });
-}
+
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+const server = http.createServer(app);
+mongoose.connect(process.env.mongo_url).then(() => {
+  console.log("Mongodb connected");
+  server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
+}).catch((err) => {
+  console.log({ err });
+  process.exit(1);
 });
+
+
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
